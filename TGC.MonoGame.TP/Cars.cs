b9b371fbefa00bb78968d1fd0321b42a0e73f1d5
Tincho2003 <martincho.cabrera22@gmail.com>
2024-09-10 17:44:16 -0300

    carpeta autos ordenada

--- TGC.MonoGame.TP/Cars.cs
+++ TGC.MonoGame.TP/Cars.cs
@@ -18,11 +18,18 @@ namespace TGC.MonoGame.TP.Content.Models
         public const string ContentFolderEffects = "Effects/";
 
         private Model CarModel { get; set; }
+        private Model CartoonCar { get; set; }
+        private Model flatoutCar { get; set; }
+        private Model deLorean { get; set; }
+        private Model kombi { get; set; }
+        private Model combatVehicle { get; set; }
+        
         private Effect EffectCar { get; set; }
         private List<Vector3> traslaciones { get; set; }
 
-         private List<float> angulosHaciaCentro { get; set; }
+        private List<float> angulosHaciaCentro { get; set; }
         private int CantAutos { get; set; }
+        public List<Model> listaModelos {get; set;}
 
         // <summary>
         /// Creates a Car Model with a content manager to load resources.
@@ -30,14 +37,19 @@ namespace TGC.MonoGame.TP.Content.Models
         /// <param name="content">The Content Manager to load resources</param>
         public Cars (ContentManager content)
         {
-            CantAutos = 30;
+            CantAutos = 80;
             // Load the Car Model
-            CarModel = content.Load<Model>(ContentFolder3D+"RacingCarA/RacingCar");
+            CarModel = content.Load<Model>(ContentFolder3D+"autos/RacingCarA/RacingCar");
+            //CartoonCar = content.Load<Model>(ContentFolder3D+"autos/cartoonCar/carton_car");
+            //flatoutCar = content.Load<Model>(ContentFolder3D+"autos/flatOutCar/Car");
+            //deLorean = content.Load<Model>(ContentFolder3D+"autos/DeLorean");
+            //kombi = content.Load<Model>(ContentFolder3D+"autos/kombi2");
+            combatVehicle = content.Load<Model>(ContentFolder3D+"autos/CombatVehicle/Vehicle");
 
             // Load an effect that will be used to draw the scene
             EffectCar = content.Load<Effect>(ContentFolderEffects + "DiffuseColor");
             
-            traslaciones = GenerarPuntosEnCirculo(CantAutos, 800f);
+            traslaciones = GenerarPuntosEnCirculo(CantAutos, 1200f);
 
             angulosHaciaCentro = CalcularAngulosHaciaCentro(traslaciones);
 
@@ -50,6 +62,33 @@ namespace TGC.MonoGame.TP.Content.Models
                 {
                     meshPart.Effect = EffectCar;
                 }
+            }
+
+            foreach (var mesh in combatVehicle.Meshes)
+            {
+                // Un mesh puede tener mas de 1 mesh part (cada 1 puede tener su propio efecto).
+                foreach (var meshPart in mesh.MeshParts)
+                {
+                    meshPart.Effect = EffectCar;
+                }
+            }
+
+
+            listaModelos = new List<Model>();
+            for (int i = 0; i < CantAutos/2; i++)
+            {
+                listaModelos.Add(CarModel);
+                listaModelos.Add(combatVehicle);
+            }
+
+            // Mezclar la lista de modelos
+            var random = new Random(0);
+            for (int i = listaModelos.Count - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                var temp = listaModelos[i];
+                listaModelos[i] = listaModelos[j];
+                listaModelos[j] = temp;
             }
 
         }
@@ -65,10 +104,16 @@ namespace TGC.MonoGame.TP.Content.Models
             EffectCar.Parameters["View"].SetValue(view);
             EffectCar.Parameters["Projection"].SetValue(projection);
             
-            var random = new Random(Seed:0);
-            
-            
+            var meshBaseCombatVehicle = new Matrix[combatVehicle.Bones.Count];
+            combatVehicle.CopyAbsoluteBoneTransformsTo(meshBaseCombatVehicle);
+            var meshBaseModelCar = new Matrix[CarModel.Bones.Count];
+            CarModel.CopyAbsoluteBoneTransformsTo(meshBaseModelCar);
+           
 
+            var world = Matrix.Identity;
+
+
+            var random = new Random(Seed:0);
 
             for (int i = 0; i < CantAutos; i++){
                 
@@ -76,6 +121,7 @@ namespace TGC.MonoGame.TP.Content.Models
                 // var colorcito = new Vector3((CameraPosition.X) + random.NextSingle(), CameraPosition.Y + random.NextSingle(), CameraPosition.Z + random.NextSingle());
                 var color = new Vector3(random.NextSingle(), random.NextSingle(), random.NextSingle());         /*Usamos verto3 porque es BasicEffect. Se usa vector4 si tenemos activado el AlphaShader*/
 
+                var angulo = angulosHaciaCentro[i];
                 /*var traslacion = new Vector3(
                 -1000f + (100f - (-1000f)) * random.NextSingle(),
                 0,
@@ -85,11 +131,16 @@ namespace TGC.MonoGame.TP.Content.Models
                 //SCALA DE 0.5 A 1
                 //-770 A 650
                 //-1200 A 1000
-                foreach (var mesh in CarModel.Meshes)
-                {
-                    
+
+
+                foreach (var mesh in listaModelos[i].Meshes)
+                {   
+                    if (listaModelos[i] == combatVehicle){
+                        scala = 0.004f + (0.004f - 0.001f) * random.NextSingle();
+                        angulo =   angulosHaciaCentro[i] + (float)Math.PI/2;
+                    }
                     EffectCar.Parameters["DiffuseColor"].SetValue(color);
-                    EffectCar.Parameters["World"].SetValue(mesh.ParentBone.Transform * Matrix.CreateRotationY(angulosHaciaCentro[i]) * Matrix.CreateScale(scala) * Matrix.CreateTranslation(traslaciones[i]) );
+                    EffectCar.Parameters["World"].SetValue(mesh.ParentBone.Transform * Matrix.CreateRotationY(angulo) * Matrix.CreateScale(scala) * Matrix.CreateTranslation(traslaciones[i]) );
 
                     mesh.Draw();
                 }
